88105d14eb019fef7fc4302c62f5bc8968c12c7f
pwn4justice <20819473@qq.com>
2020-03-26 21:58:48 +0800

    update-stdio

---
+++ 3_buffered_io/stdio/archive/fwrite/training_fwrite.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* size_t fwrite (void *buf, size_t size, size_t nr, FILE *stream)
+ *
+ * DESCRIPTION:
+ * 	size == sizeof( *buf )
+ * 	nr == number of size
+ * 	write size*nr bytes to stream
+ *
+ * RETURN:
+ * 	nr if success
+ * 	<nr if failed
+ */
+ 
